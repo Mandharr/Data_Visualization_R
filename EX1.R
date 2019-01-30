d922607494a98a18451f83bc978088a57a47440c
Mandar Bhosale <mandharr.bhoslay19@gmail.com>
2019-01-30 01:48:36 -0500

    Scatter PLot and Histogram Added

--- EX1.R
+++ EX1.R
@@ -86,3 +86,73 @@ ggplot(mtcars, aes(x=wt, y=mpg, col=cyl)) + geom_point()
 ggplot(mtcars, aes(x=wt, y=mpg, col=cyl)) + geom_point(color=my_color)
 
 ggplot(mtcars, aes(x=wt, y=mpg, fill=cyl)) + geom_point(size=10, shape=23, color=my_color)
+
+# Adding the geom_text() for inserting label instead of points
+# Warning: Bad Plot
+
+ggplot(mtcars, aes(x = wt, y = mpg, fill = cyl)) + geom_text(label=rownames(mtcars), color= "red")
+
+# Working on aesthetic mapping
+
+ggplot(mtcars, aes(x=mpg, y=qsec, color=factor(cyl),shape=factor(am), size= hp/wt)) + geom_point()
+
+
+# Modifying Aesthetics
+cyl.am <- ggplot(mtcars, aes(x = factor(cyl), fill = factor(am)))
+val = c("#E41A1C", "#377EB8")
+lab = c("Manual", "Automatic")
+cyl.am +
+  geom_bar(position = "dodge") +
+  scale_x_discrete("Cylinders") + 
+  scale_y_continuous("Number") +
+  scale_fill_manual("Transmission", 
+                    values = val,
+                    labels = lab) 
+
+# Overcoming Overplotting
+
+ggplot(diamonds, aes(x=clarity, y= carat, color=price)) + geom_point(alpha=0.5, position="jitter")
+
+
+# Scatter PLot with Jittering
+
+ggplot(Vocab, aes(x=education, y=vocabulary))+geom_jitter(alpha=0.2, shape=1)
+
+# Plotting Histograms
+
+ggplot(mtcars, aes(x = mpg)) +
+  geom_histogram(aes(y= ..density..),binwidth= 1, fill = "#377EB8")
+
+
+# Draw a bar plot of cyl, filled according to am
+ggplot(mtcars, aes(x =cyl, fill = am)) + geom_bar()
+
+# Change the position argument to stack
+ggplot(mtcars, aes(x =cyl, fill = am)) + geom_bar(position = "stack")
+
+
+# Change the position argument to fill
+ggplot(mtcars, aes(x =cyl, fill = am)) + geom_bar(position = "fill")
+
+
+# Change the position argument to dodge
+ggplot(mtcars, aes(x =cyl, fill = am)) + geom_bar(position = "dodge")
+
+# A basic histogram, add coloring defined by cyl
+ggplot(mtcars, aes(mpg,fill=cyl)) +
+  geom_histogram(binwidth = 1)
+
+# Change position to identity
+ggplot(mtcars, aes(mpg,fill=cyl)) +
+  geom_histogram(binwidth = 1, position="identity")
+
+
+# Change geom to freqpoly (position is identity by default)
+ggplot(mtcars, aes(mpg,color=cyl)) +
+  geom_freqpoly()
+
+# Plot education on x and vocabulary on fill
+# Use the default brewed color palette
+ggplot(Vocab, aes(x = education, fill = vocabulary)) +
+  geom_bar(position = "fill") +
+  scale_fill_brewer()
